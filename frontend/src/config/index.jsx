25f427974a53403d72cc51dbd803127a91163390
Dattatray Savalkar <dattatraysavalkar1705@gmail.com>
2026-08-02 20:05:33 +0530

    chnage backend url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-
-export const BASE_URL = "http://localhost:5000"; // 👈 backend port
 export const clientServer = axios.create({
-  baseURL: BASE_URL, // 👈 backend port
+  baseURL: "https://connectx-backend-888g.onrender.com",
   withCredentials: true,
 });
