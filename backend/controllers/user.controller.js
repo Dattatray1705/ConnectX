c2b3b6e71c2d666804e649c42b005c2ce4200114
Dattatray Savalkar <dattatraysavalkar1705@gmail.com>
2026-08-02 23:22:45 +0530

    Add start script

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -146,19 +146,13 @@ console.log(
   process.env.EMAIL_PASS ? "FOUND" : "NOT FOUND"
 );
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
- port: 587,
-secure: false,
-requireTLS: true,
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     console.log("4. Transport Created");
 
